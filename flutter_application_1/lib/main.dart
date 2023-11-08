e39fc94053bb60d6e0f8f9637e84ea1f5951ee8c
anhdv <doanvietanhyolo97@gmail.com>
2023-11-08 11:41:04 +0700

    feat: update UI

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,125 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:mysql1/mysql1.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Desktop App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Initialize variables for database connectivity, data, and controllers
+  final _formKey = GlobalKey<FormState>();
+  List<Map<String, dynamic>> _queryResults = [];
+  // Database configuration
+  // final conn = MySqlConnection.connect(ConnectionSettings(
+  //   host: 'localhost',
+  //   port: 3306,
+  //   user: 'root',
+  //   db: 'app_01',
+  //   password: '!J7X`_;@p\n+3wJx',
+  // ));
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // // Function to insert data into MySQL
+  // Future<void> _insertData(String data) async {
+  //   final conn = await this.conn;
+  //   // Insert data into the database
+  // }
+
+  // // Function to query and filter data from MySQL
+  // Future<void> _queryData(String filter) async {
+  //   final conn = await this.conn;
+  //   // Query the database with filtering
+  // }
+
+  // Function to upload PDF
+  Future<void> _uploadPdf() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      File file = File(result.files.single.path!);
+      // Upload file to the server or database
+    }
+  }
+
+  // Function to export data to PDF
+  Future<void> _exportToPdf(List<Map<String, dynamic>> data) async {
+    PdfDocument document = PdfDocument();
+    // Populate the PDF file with data
+    // Save the document
+  }
+
+  // Function to get chart data
+  List<FlSpot> _getChartData(List<Map<String, dynamic>> data) {
+    // Convert data to chart points
+    return [];
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Flutter Desktop App'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              // Form fields to input data
+              // TextFormField(
+              //   decoration: InputDecoration(labelText: 'Name'),
+              //   validator: (value) {
+              //     if (value.isEmpty) {
+              //       return 'Please enter a name';
+              //     }
+              //     return null;
+              //   },
+              // ),
+              // Add more form fields as needed
+
+              // Buttons to save data, query data, upload PDF, export PDF
+              ElevatedButton(
+                onPressed: () {
+                  // Handle the save data logic
+                },
+                child: Text('Save Data'),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Handle the query data logic
+                },
+                child: Text('Query Data'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle the upload PDF logic
+                },
+                child: Text('Upload PDF'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle the export PDF logic
+                },
+                child: Text('Export PDF'),
+              ),
+
+              // Display chart with data
+              // Example using the fl_chart library:
+              // LineChart(
+              //   LineChartData(
+              //     // Define your chart data here
+              //     // You can customize the chart appearance and data points
+              //     // Visit the fl_chart documentation for more details
+              //   ),
+              // ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
